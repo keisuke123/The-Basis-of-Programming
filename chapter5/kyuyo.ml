4cf794be1d28842b190aed2c0649437b932d6164
Kamiya Keisuke <ozilikepop@gmail.com>
2019-04-20 00:23:37 +0900

    優遇給与

---
+++ chapter5/kyuyo.ml
@@ -0,0 +1,19 @@
+(* 時給 *)
+let jikyu = 950
+
+(* 基本給（円） *)
+let kihonkyu = 100
+
+(* 30時間以上 *)
+let yugu_jikyu = 980
+
+let kyuyo x = 
+    if x < 30 then
+      kihonkyu + x * jikyu
+    else
+      kihonkyu + x * yugu_jikyu
+
+(* テスト *)
+let test1 = kyuyo 25 = 23850
+let test2 = kyuyo 28 = 26700
+let test3 = kyuyo 31 = 30480
