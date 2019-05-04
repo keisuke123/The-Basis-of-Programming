52d32e437924df62517e2de7aedb05a5c52f3a98
Kamiya Keisuke <ozilikepop@gmail.com>
2019-05-04 21:23:30 +0900

    問題8.1の解答追加

---
+++ chapter8/problem8-1.ml
@@ -0,0 +1,11 @@
+type book_t = {
+  title: string;
+  author: string;
+  price: int;
+  publisher: string;
+  isbn: int;
+}
+
+let book1 = {title="かるめり"; author="かるめり"; price=0; publisher="かるめり"; isbn=4798155659}
+let book2 = {title="あ"; author="あ"; price=0; publisher="あ"; isbn=4798155659}
+let book3 = {title="お"; author="お"; price=0; publisher="お"; isbn=4798155659}
