27705045958d29d93c0ce5e7b9b9e6bd1e8fc22f
Kamiya Keisuke <ozilikepop@gmail.com>
2019-05-04 21:26:01 +0900

    問題8.2の解答追加

---
+++ chapter8/problem8-2.ml
@@ -0,0 +1,10 @@
+type okozukai_t = {
+  name: string;
+  price: int;
+  place: string;
+  date: string;
+}
+
+let okozukai1 = {name="ほげほげ"; price=100000; place="here"; date="2020/2/2/"}
+let okozukai2 = {name="ほげほげ"; price=2000; place="here"; date="2020/2/2/"}
+let okozukai3 = {name="ほげほげ"; price=1000; place="here"; date="2020/2/2/"}
