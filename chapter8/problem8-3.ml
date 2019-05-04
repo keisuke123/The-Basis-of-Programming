0be1619dc4f973864eee072f8000d81a6e7d2371
Kamiya Keisuke <ozilikepop@gmail.com>
2019-05-04 21:28:35 +0900

    問題8.3の解答追加

---
+++ chapter8/problem8-3.ml
@@ -0,0 +1,10 @@
+type person_t = {
+  height: int;
+  weight: int;
+  birthday: string;
+  bloodType: string;
+}
+
+let person1 = {height=175; weight=175; birthday="2020/1/1"; bloodType="A"}
+let person2 = {height=165; weight=175; birthday="2020/1/1"; bloodType="A"}
+let person3 = {height=152; weight=45; birthday="1975/11/22"; bloodType="AB"}
