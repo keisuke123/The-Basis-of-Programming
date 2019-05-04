67735a9d36d2e7fd16a3cc7d43686802d0ec6935
Kamiya Keisuke <ozilikepop@gmail.com>
2019-05-04 21:47:46 +0900

    フィールドが足りていない

--- chapter8/problem8-3.ml
+++ chapter8/problem8-3.ml
@@ -1,10 +1,11 @@
-type person_t = {
+type person_t = { 
+  name: string;
   height: int;
   weight: int;
   birthday: string;
   bloodType: string;
 }
 
-let person1 = {height=175; weight=175; birthday="2020/1/1"; bloodType="A"}
-let person2 = {height=165; weight=175; birthday="2020/1/1"; bloodType="A"}
-let person3 = {height=152; weight=45; birthday="1975/11/22"; bloodType="AB"}
+let person1 = {name="hoge"; height=175; weight=175; birthday="2020/1/1"; bloodType="A"}
+let person2 = {name="fuga"; height=165; weight=175; birthday="2020/1/1"; bloodType="A"}
+let person3 = {name="foo"; height=152; weight=45; birthday="1975/11/22"; bloodType="AB"}
