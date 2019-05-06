a659f9c42946df5ca7e78616493840984e679a9f
Kamiya Keisuke <ozilikepop@gmail.com>
2019-05-06 23:30:51 +0900

    デザインレシピ

---
+++ chapter9/design-recipe.ml
@@ -0,0 +1,15 @@
+(* int lst は
+    - []            空リスト
+    - first :: rest 最初の要素がfirstで残りのリストがrest (restが自己参照) *)
+
+(* 目的：受け取ったリストはlstの各要素の和を求める *)
+(* sum : int list -> int *)
+let rec sum lst = match lst with
+                [] -> 0
+              | first :: rest -> first + sum rest (* sum rest *)
+
+(* テスト *)
+let test1 = sum [] = 0
+let test2 = sum [2] = 2
+let test3 = sum [1; 2] = 3
+let test4 = sum [1; 2; 3; 4; 5; 6; 7; 8; 9; 10] = 55
